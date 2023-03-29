4ae86d871ac2159b546e1e9e31905fa94bfcbf97
omanias <ojmanias1985@gmail.com>
2023-03-28 21:16:29 -0300

    clase1 added

---
+++ clase1.js
@@ -0,0 +1,79 @@
+// VARIABLES
+
+/* let comision = 39515 //valor numerico
+comision = "otro numero"
+comision = true
+let alumno = "Carlos" //valor cadena o string
+
+let cursando = true // valor booleano -> true o false */
+
+// console.log(comision)
+/* console.log(alumno)
+console.log(cursando) */
+
+
+/* const marca = "Samsung"
+// marca = 39515
+
+console.log(marca) */
+
+//LET permite reasignar valores a una variable mientras que CONST no lo permite
+// CONST no puede ser inicializada vacia
+
+/* let comision;
+
+
+comision = 39515 */
+
+
+/* const comision = 39515
+
+const bienvenida = "Bienvenidos"
+
+//CONCATENACION
+
+const unidos = bienvenida + " " + comision
+
+const numero1 = "656"
+
+const numero2 = 789
+
+console.log(numero1 + numero2) */
+
+
+// PROMPT
+
+/* const nombre = prompt("Ingrese su nombre")
+
+const apellido = prompt("Ingrese su apellido")
+
+const bienvenida = "Bienvenido " + nombre + " " + apellido
+
+alert(bienvenida) */
+
+// console.log(bienvenida)
+
+// +
+// -
+// *
+// /
+
+//OPERADORES ARITMETICOS
+
+/* const numero1 = prompt("Ingrese el primer número")
+const numero2 = prompt("Ingrese el segundo número")
+
+const multiplicacion = numero1 * numero2
+const division = numero1 / numero2
+const suma = parseInt(numero1) + parseInt(numero2)
+const resta = numero1 - numero2
+// alert("El resultado de la suma es: " + suma)
+
+console.log(typeof(numero1)) */
+
+
+
+
+
+
+
